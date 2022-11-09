3e34c0c91938523589f890d4994bc79191c537d5
mubeena <mubeenahamza@gmail.com>
2022-11-09 17:46:06 +0100

    add clear btn & use cookie-session

--- public/scripts/canvas.js
+++ public/scripts/canvas.js
@@ -1,102 +1,106 @@
-(function () {
-    var canvas,
-        ctx,
-        flag = false,
-        prevX = 0,
-        currX = 0,
-        prevY = 0,
-        currY = 0,
-        dot_flag = false;
+var canvas,
+    ctx,
+    flag = false,
+    prevX = 0,
+    currX = 0,
+    prevY = 0,
+    currY = 0,
+    dot_flag = false;
 
-    var x = "black",
-        y = 2;
+var x = "black",
+    y = 2;
 
-    canvas = document.getElementById("canvas_signature");
-    ctx = canvas.getContext("2d");
-    w = canvas.width;
-    h = canvas.height;
+canvas = document.getElementById("canvas_signature");
+ctx = canvas.getContext("2d");
+w = canvas.width;
+h = canvas.height;
 
-    canvas.addEventListener(
-        "mousemove",
-        function (e) {
-            findxy("move", e);
-        },
-        false
-    );
-    canvas.addEventListener(
-        "mousedown",
-        function (e) {
-            findxy("down", e);
-        },
-        false
-    );
-    canvas.addEventListener(
-        "mouseup",
-        function (e) {
-            findxy("up", e);
-        },
-        false
-    );
-    canvas.addEventListener(
-        "mouseout",
-        function (e) {
-            findxy("out", e);
-        },
-        false
-    );
+canvas.addEventListener(
+    "mousemove",
+    function (e) {
+        findxy("move", e);
+    },
+    false
+);
+canvas.addEventListener(
+    "mousedown",
+    function (e) {
+        findxy("down", e);
+    },
+    false
+);
+canvas.addEventListener(
+    "mouseup",
+    function (e) {
+        findxy("up", e);
+    },
+    false
+);
+canvas.addEventListener(
+    "mouseout",
+    function (e) {
+        findxy("out", e);
+    },
+    false
+);
 
-    function draw() {
-        ctx.beginPath();
-        ctx.moveTo(prevX, prevY);
-        ctx.lineTo(currX, currY);
-        ctx.strokeStyle = x;
-        ctx.lineWidth = y;
-        ctx.stroke();
-        ctx.closePath();
+function draw() {
+    ctx.beginPath();
+    ctx.moveTo(prevX, prevY);
+    ctx.lineTo(currX, currY);
+    ctx.strokeStyle = x;
+    ctx.lineWidth = y;
+    ctx.stroke();
+    ctx.closePath();
 
-        save();
+    save();
+}
+
+function erase(e) {
+    console.log("erase");
+    e.preventDefault();
+    ctx.clearRect(0, 0, w, h);
+    document.getElementById("canvasimg").style.display = "none";
+}
+
+function save() {
+    document.getElementById("canvasimg").style.border = "2px solid";
+    var dataURL = canvas.toDataURL();
+    document.getElementById("canvasimg").value = dataURL;
+    // document.getElementById("canvasimg").style.display = "inline";
+}
+
+function findxy(res, e) {
+    if (res == "down") {
+        prevX = currX;
+        prevY = currY;
+        currX = e.clientX - canvas.offsetLeft;
+        currY = e.clientY - canvas.offsetTop;
+
+        flag = true;
+        dot_flag = true;
+        if (dot_flag) {
+            ctx.beginPath();
+            ctx.fillStyle = x;
+            ctx.fillRect(currX, currY, 2, 2);
+            ctx.closePath();
+            dot_flag = false;
+        }
     }
-
-    function erase() {
-        ctx.clearRect(0, 0, w, h);
-        document.getElementById("canvasimg").style.display = "none";
+    if (res == "up" || res == "out") {
+        flag = false;
     }
-
-    function save() {
-        document.getElementById("canvasimg").style.border = "2px solid";
-        var dataURL = canvas.toDataURL();
-        document.getElementById("canvasimg").value = dataURL;
-        // document.getElementById("canvasimg").style.display = "inline";
-    }
-
-    function findxy(res, e) {
-        if (res == "down") {
+    if (res == "move") {
+        if (flag) {
             prevX = currX;
             prevY = currY;
             currX = e.clientX - canvas.offsetLeft;
             currY = e.clientY - canvas.offsetTop;
-
-            flag = true;
-            dot_flag = true;
-            if (dot_flag) {
-                ctx.beginPath();
-                ctx.fillStyle = x;
-                ctx.fillRect(currX, currY, 2, 2);
-                ctx.closePath();
-                dot_flag = false;
-            }
-        }
-        if (res == "up" || res == "out") {
-            flag = false;
-        }
-        if (res == "move") {
-            if (flag) {
-                prevX = currX;
-                prevY = currY;
-                currX = e.clientX - canvas.offsetLeft;
-                currY = e.clientY - canvas.offsetTop;
-                draw();
-            }
+            draw();
         }
     }
-})();
+}
+
+document.getElementById("clear_canvas").addEventListener("click", (event) => {
+    erase(event);
+});
--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const cookieParser = require("cookie-parser");
-const session = require("express-session");
+// const session = require("express-session");
+const cookieSession = require("cookie-session");
 
 const { addPetitioner, getPetitionersCount, getPetitioners } = require("./db");
-const { convertURIToImageData } = require("./public/utils/uri_to_image");
 
 //handlerbar
 const handlebars = require("express-handlebars");
@@ -15,8 +15,18 @@ app.set("view engine", "handlebars");
 // install middleware to help us read POST body (form data) easily
 app.use(express.urlencoded({ extended: false }));
 
+// app.use(
+//     session({ secret: "mySecret", resave: false, saveUninitialized: false })
+// );
+
 app.use(
-    session({ secret: "mySecret", resave: false, saveUninitialized: false })
+    cookieSession({
+        name: "session",
+        keys: ["I am hungry"],
+
+        // Cookie Options
+        maxAge: 1000 * 60 * 60 * 24 * 14, // 24 * 14 hours
+    })
 );
 
 app.get("/petition", (req, res) => {
@@ -30,31 +40,27 @@ app.get("/petition", (req, res) => {
 app.post("/petition", (req, res) => {
     const submit = req.body.submit;
     //check if submit button clicked
-    if (submit === "submit") {
-        // check fields
-        if (
-            !req.body.first_name ||
-            !req.body.last_name ||
-            !req.body.signatureUrl
-        ) {
-            return res.render("petition_form", { error: "Fill all fields" });
-        }
 
-        addPetitioner({
-            firstName: req.body.first_name,
-            lastName: req.body.last_name,
-            signature: req.body.signatureUrl,
-        }).then((result) => {
-            //set cookies
-            res.cookie("signed", true);
-            req.session.petitioner = result;
-            return res.redirect("/signed");
-        });
+    // check fields
+    if (!req.body.first_name || !req.body.last_name || !req.body.signatureUrl) {
+        return res.render("petition_form", { error: "Fill all fields" });
     }
+
+    addPetitioner({
+        firstName: req.body.first_name,
+        lastName: req.body.last_name,
+        signature: req.body.signatureUrl,
+    }).then((result) => {
+        //set cookies
+        res.cookie("signed", true);
+        req.session.petitioner = result;
+        return res.redirect("/signed");
+    });
 });
 
 app.get("/signed", (req, res) => {
     const petitioner = req.session.petitioner;
+    console.log("petio: ", petitioner);
     getPetitionersCount().then((count) => {
         res.render("petition_signed", { count: count, petitioner });
     });
