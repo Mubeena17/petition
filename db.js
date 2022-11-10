1f9d4151dc4dfe04f375cbf3d092141d1f0bf351
mubeena <mubeenahamza@gmail.com>
2022-11-10 15:13:07 +0100

    login and signup

--- db.js
+++ db.js
@@ -7,16 +7,16 @@ const database = "petition";
 const db = spicedPg(`postgres:${user}:${password}@localhost:5432/${database}`);
 
 module.exports.getPetitioners = () => {
-    return db.query("SELECT * FROM signatures").then((result) => result.rows);
+    return db.query("SELECT * FROM users").then((result) => result.rows);
 };
-
-module.exports.addPetitioner = ({ firstName, lastName, signature }) => {
+//signup
+module.exports.addPetitioner = ({ firstName, lastName, email, password }) => {
     return db
         .query(
-            `INSERT INTO signatures ("first name", "last name", "signature")
-    VALUES ($1, $2, $3)
-    RETURNING *`,
-            [firstName, lastName, signature]
+            `INSERT INTO users ("first_name", "last_name", "email", "password")
+    VALUES ($1, $2, $3, $4)
+    RETURNING id`,
+            [firstName, lastName, email, password]
         )
         .then((result) => result.rows[0]);
 };
@@ -25,4 +25,44 @@ module.exports.getPetitionersCount = () => {
     return db
         .query("SELECT COUNT(signature) FROM signatures")
         .then((result) => result.rows[0].count);
+};
+
+module.exports.getUserDetail = (email, password) => {
+    return db
+        .query(
+            `SELECT first_name, last_name, id FROM users WHERE email=$1 AND password=$2`,
+            [email, password]
+        )
+        .then((result) => result.rows[0]);
+};
+
+module.exports.authenticateUser = (email, password) => {
+    return db
+        .query(
+            `SELECT first_name, last_name, id FROM users WHERE email=$1 AND password=$2`,
+            [email, password]
+        )
+        .then((result) => result.rows[0]);
+};
+
+module.exports.getCurrentUserDetails = (user_id) => {
+    return db
+        .query(`SELECT * FROM users WHERE id=$1`, [user_id])
+        .then((result) => result.rows[0]);
+};
+
+module.exports.addSignature = ({ user_id, signature }) => {
+    console.log("uswerid ::::", user_id);
+    return db
+        .query(
+            `INSERT INTO signatures ("user_id", "signature") VALUES ($1, $2) RETURNING id`,
+            [user_id, signature]
+        )
+        .then((result) => result.rows[0]);
+};
+
+module.exports.getSignature = (user_id) => {
+    return db
+        .query(`SELECT signature FROM signatures WHERE user_id=$1 `, [user_id])
+        .then((result) => result.rows[0]);
 };
--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const cookieParser = require("cookie-parser");
-// const session = require("express-session");
 const cookieSession = require("cookie-session");
-
-const { addPetitioner, getPetitionersCount, getPetitioners } = require("./db");
+const {
+    addPetitioner,
+    getPetitionersCount,
+    getPetitioners,
+    authenticateUser,
+    getCurrentUserDetails,
+    addSignature,
+    getSignature,
+} = require("./db");
 
 //handlerbar
 const handlebars = require("express-handlebars");
@@ -15,26 +20,99 @@ app.set("view engine", "handlebars");
 // install middleware to help us read POST body (form data) easily
 app.use(express.urlencoded({ extended: false }));
 
-// app.use(
-//     session({ secret: "mySecret", resave: false, saveUninitialized: false })
-// );
-
 app.use(
     cookieSession({
         name: "session",
         keys: ["I am hungry"],
-
-        // Cookie Options
         maxAge: 1000 * 60 * 60 * 24 * 14, // 24 * 14 hours
     })
 );
 
-app.get("/petition", (req, res) => {
-    //check here if logged
-    //else cookies section create
+//check for session cookies
+function auth(req, res, next) {
+    if (!req.session.user_id) {
+        return res.redirect("/login");
+    } else {
+        next();
+    }
+}
 
-    //res.render(handlebarname)
-    return res.render("petition_form");
+//App start
+app.get("/", auth, (req, res) => {
+    return res.redirect("/petition");
+});
+
+app.get("/login", (req, res) => {
+    return res.render("login_form");
+});
+
+// login action
+app.post("/login", (req, res) => {
+    const { email, password } = req.body;
+
+    // authenticateUser() returns a Promise
+    // if the authentication is successful, the promise is resolved
+    // with the logged in user (object)
+    // if authentication fails, the promise is rejected
+    authenticateUser(email, password)
+        .then((user) => {
+            // store the id of the logged in user inside the session cookie
+            req.session.user_id = user.id;
+            res.redirect("/petition");
+        })
+        .catch(() => {
+            res.render("login", {
+                message: "Login failed!",
+            });
+        });
+});
+//signup route
+app.get("/signup", (req, res) => {
+    return res.render("signup_form");
+});
+
+//signup post
+app.post("/signup", (req, res) => {
+    // check fields
+    const { first_name, last_name, email, password } = req.body;
+    if (!first_name || !last_name || !email || !password) {
+        return res.render("signup_form", { error: "Fill all fields" });
+    }
+    addPetitioner({
+        firstName: first_name,
+        lastName: last_name,
+        email: email,
+        password: password,
+    }).then((user) => {
+        //set session user_id
+        req.session.user_id = user.id;
+        return res.redirect("/petition");
+    });
+});
+
+//logout
+app.get("/logout", (req, res) => {
+    req.session = null;
+    res.clearCookie("signed");
+    return res.redirect("/petition");
+});
+
+app.get("/petition", (req, res) => {
+    user_id = req.session.user_id;
+    let currentUser;
+    //grab all detail
+    getCurrentUserDetails(user_id)
+        .then((user) => {
+            req.session.first_name = user.first_name;
+            req.session.last_name = user.last_name;
+            currentUser = {
+                firstName: user.first_name,
+                lastName: user.last_name,
+                user_id: user_id,
+            };
+            return currentUser;
+        })
+        .then((user) => res.render("petition_form", { user }));
 });
 
 app.post("/petition", (req, res) => {
@@ -42,28 +120,40 @@ app.post("/petition", (req, res) => {
     //check if submit button clicked
 
     // check fields
-    if (!req.body.first_name || !req.body.last_name || !req.body.signatureUrl) {
+    if (!req.body.signatureUrl) {
         return res.render("petition_form", { error: "Fill all fields" });
     }
 
-    addPetitioner({
-        firstName: req.body.first_name,
-        lastName: req.body.last_name,
+    addSignature({
+        user_id: req.session.user_id,
+        // signature: "req.body.signatureUrl",
         signature: req.body.signatureUrl,
     }).then((result) => {
         //set cookies
         res.cookie("signed", true);
-        req.session.petitioner = result;
+        // not working req.session.signature = req.body.signatureUrl;
         return res.redirect("/signed");
     });
 });
 
 app.get("/signed", (req, res) => {
-    const petitioner = req.session.petitioner;
-    console.log("petio: ", petitioner);
-    getPetitionersCount().then((count) => {
-        res.render("petition_signed", { count: count, petitioner });
-    });
+    let user;
+    getSignature(req.session.user_id)
+        .then((result) => {
+            user = {
+                first_name: req.session.first_name,
+                last_name: req.session.last_name,
+                signature: result.signature,
+            };
+        })
+        .then(
+            getPetitionersCount().then((count) => {
+                return res.render("petition_signed", {
+                    count: count,
+                    petitioner: user,
+                });
+            })
+        );
 });
 
 app.get("/signers", (req, res) => {
